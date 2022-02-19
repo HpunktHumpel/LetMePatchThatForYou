70f07551a877d7a1a785b0df1087dcfe8587a8e8
H.Humpel <H.Humpel@gmx.de>
2022-02-19 20:11:21 +0100

    [FIXED] Lyceum patch

--- LetMePatchThatForYou/1871766085/Lyceum_Text_patch.sql
+++ LetMePatchThatForYou/1871766085/Lyceum_Text_patch.sql
@@ -3,5 +3,9 @@ INSERT OR REPLACE INTO LocalizedText
 VALUES
     ('en_US',	'LOC_BUILDING_LYCEUM_NAME',				  'Lyceum'),
     ('en_US',	'LOC_BUILDING_LYCEUM_DESCRIPTION',	'Provides +2 [ICON_Science] Science in this city each turn.'),
+		('en_US', 'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_LYCEUM_CHAPTER_HISTORY_PARA_1',
+							'The temple of Apollo Lyceus in ancient Athens often doubled as the site of philosophical discussions, most famously those of Aristotle and the Peripatetics.'),
     ('de_DE',	'LOC_BUILDING_LYCEUM_NAME',				  'Lyzeum'),
-    ('de_DE',	'LOC_BUILDING_LYCEUM_DESCRIPTION',	'Biete +2 [ICON_Science] Wissenschaft pro Runde in dieser Stadt.');
+    ('de_DE',	'LOC_BUILDING_LYCEUM_DESCRIPTION',	'Biete +2 [ICON_Science] Wissenschaft pro Runde in dieser Stadt.'),
+		('de_DE', 'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_LYCEUM_CHAPTER_HISTORY_PARA_1',
+							'Der Tempel des Apollo Lyceus im antiken Athen diente oft als Ort philosophischer Diskussionen, vor allem der von Aristoteles und den Peripatetikern.');
